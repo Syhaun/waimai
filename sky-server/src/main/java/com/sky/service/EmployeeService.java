2e4f45fd4c635c846b5bb5fa7c5d439060153a47
Syhaun <2776299759@qq.com>
2024-05-21 16:20:14 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -17,4 +19,11 @@ public interface EmployeeService {
      * @param employeeLoginDTO
      */
     void sava(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
